fbcbc6e7741a47cbe29eeba826be17bd62608397
ynmth <90394926+ynmth@users.noreply.github.com>
2024-06-07 23:39:01 +0900

    Update crepe_pitch_model.py

--- model/crepe_pitch_model.py
+++ model/crepe_pitch_model.py
@@ -10,13 +10,11 @@ MIDI_TO_NOTE = {
 
 def extract_pitch_crepe(wav_file, step_size=10, model_capacity='full'):
     """
-    CREPE 모델을 사용하여 WAV 파일에서 피치를 추출합니다.
-
     :param wav_file: 분석할 WAV 파일 경로
     :param step_size: CREPE 모델의 스텝 크기
     :param model_capacity: CREPE 모델 용량 ('tiny', 'small', 'medium', 'large', 'full')
     :return: time, frequency, confidence
     """
-    y, sr = librosa.load(wav_file, sr=16000)  # CREPE는 16kHz 샘플링을 사용
+    y, sr = librosa.load(wav_file, sr=16000)
     time, frequency, confidence, activation = crepe.predict(y, sr, step_size=step_size, model_capacity=model_capacity)
     return time, frequency, confidence
